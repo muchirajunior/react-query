8ba2f535d2dc5248fb2c5b63cc6c7c7099166dd3
muchirajunior <muchirajunior022@gmail.com>
2025-05-25 23:01:51 +0300

    :gear:

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,7 @@
 export const Footer = () => {
     return (
-        <footer className="py-3 my-4 fixed-bottom container bg-light">
-            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                {/* <li className="nav-item"><a href="#" className="nav-link px-2 ">Home</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 ">Features</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 ">Pricing</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 ">FAQs</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 ">About</a></li> */}
-            </ul>
+        <footer className="py-3 mt-4 fixed-bottom container bg-light">
+           
             <p className="text-center ">© 2024 Tanstack React Query Exproler</p>
         </footer>
     )
