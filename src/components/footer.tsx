648433ff802a41ff9c44e90a854c1f9f2aaaa101
MUCHIRAJUNIOR <muchirajunior022@gmail.com>
2025-07-11 01:06:15 +0300

    docs(footer): update copyright year to 2025

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer className="py-3 mt-4 fixed-bottom container bg-light">
            
-            <p className="text-center ">© 2024 Tanstack React Query Exproler</p>
+            <p className="text-center ">© 2025 Tanstack React Query Exproler</p>
         </footer>
     )
 }
